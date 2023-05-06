e590e45c0e1f6ba2ef76f824d57e5f9475365c79
FdgCertainly <franco.giuntoli@gmail.com>
2023-05-07 00:07:08 +0200

    page display config

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,17 @@ import Dock from "./components/dock/Dock"
 import Calendar from "./components/calendar/Calendar"
 import Slider from "./components/ios-slider/Slider"
 import Steps from "./components/steps/Steps"
+import Calendar2 from "./components/calendar/Calendar2"
 
 export default function Home() {
   return (
-    <div className="flex h-[100vh] flex-col justify-between py-1">
+    <div className="flex min-h-screen flex-col justify-between bg-red-900 py-1">
       <Toggle />
-      <Steps />
-      {/* <Calendar /> */}
-      {/* <Slider /> */}
+      <div className="my-auto grid grid-cols-2">
+        <Steps />
+        <Calendar2 />
+      </div>
+
       <Dock />
     </div>
   )
