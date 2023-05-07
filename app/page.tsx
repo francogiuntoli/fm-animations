dbbc578b0a93d0d310823c95026cf79dd578f52c
FdgCertainly <franco.giuntoli@gmail.com>
2023-05-07 20:36:04 +0200

    fixed some imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Toggle from "./components/toggle/Toggle"
 import Dock from "./components/dock/Dock"
 
-import Steps from "./components/steps/Steps"
 import Calendar2 from "./components/calendar/Calendar2"
 import ResizablePanel from "./components/resizable_panel/ResizablePanel"
+import Steps from "./components/steps/Steps"
 
 export default function Home() {
   return (
