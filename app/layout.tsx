053639ebf6f0cfb835c4007343bc57b709acfdd4
FdgCertainly <franco.giuntoli@gmail.com>
2023-06-18 23:55:38 +0200

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css"
 import { Inter } from "next/font/google"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -15,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html className="bg-zinc-500" lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children} <Analytics mode={"production"} />;
+      </body>
     </html>
   )
 }
